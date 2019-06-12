6c64ef70715b2cf23581b96fb1e15c4aca6ef759
jman11111 <jbroncato@yahoo.com>
2019-06-11 19:53:11 -0500

    initial commit, work done

---
+++ MyClass.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CSharpPractice{
+    public class MyClass{
+        public String returnMessage(){
+            return "Get Yoten";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass testClass = new MyClass();
+            Console.WriteLine("How many times you want to get yeeted?:");
+            String input = Console.ReadLine();
+            int yeets = Convert.ToInt32(input);
+            for(int i = 0;i < yeets;i++){
+                Console.WriteLine(testClass.returnMessage());
+            }
+        }
+    }
+}
